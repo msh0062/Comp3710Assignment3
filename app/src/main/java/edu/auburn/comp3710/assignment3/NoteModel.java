b7bf58401b680ce0601ee74789e944ebf158f69f
Mason Hassen <msh0062@auburn.edu>
2020-02-27 16:54:52 -0600

    First commit assignmnet3

---
+++ app/src/main/java/edu/auburn/comp3710/assignment3/NoteModel.java
@@ -0,0 +1,17 @@
+package edu.auburn.comp3710.assignment3;
+
+import com.google.gson.Gson;
+
+public class NoteModel {
+    public String  title, content;
+
+    public String toJson() {
+        Gson gson = new Gson();
+        return gson.toJson(this);
+    }
+
+    public static NoteModel fromJson(String json) {
+        Gson gson = new Gson();
+        return gson.fromJson(json, NoteModel.class);
+    }
+}
